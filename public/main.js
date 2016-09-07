6704f241efa919c94e0b6c63da0f426e291c3170
Elf <sovapetro1@gmail.com>
2016-09-07 23:03:39 +0300

    the upgrade of table

--- public/main.js
+++ public/main.js
@@ -4,3 +4,14 @@ hello.addEventListener('click', function () {
   alert("Hi! I am alert from static!!");
 });
 
+angular.module('myApp', []).controller('myController', 
+function($scope, $timeout) {
+	$scope.clock = {};
+	var updateClock = function() {
+	    $scope.clock.now = new Date().toLocaleTimeString();
+	    $timeout(function() {
+	    updateClock();
+	    }, 1000);
+};
+updateClock();
+});
